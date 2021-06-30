c657736f2fbb9b5c137700940eb45aedfbc78299
Deyvi-dev <68565306+Deyvi-dev@users.noreply.github.com>
2021-06-30 00:37:07 -0300

    Delete LetmeAsk directory

--- LetmeAsk/src/components/index.tsx
+++
@@ -1,7 +0,0 @@
-import Button from './Button';
-import RoomCode from './RoomCode';
-import Question from './Question';
-import ModalDeleteRoom from './ModalDeleteRoom';
-import ModalRemoveQuestion from './ModalRemoveQuestion';
-
-export { Button, RoomCode, Question, ModalDeleteRoom, ModalRemoveQuestion };
--- LetmeAsk/src/pages/index.tsx
+++
@@ -1,6 +0,0 @@
-import Home from "./Home";
-import NewRoom from "./NewRoom";
-import Room from "./Room";
-import AdminRoom from "./AdminRoom";
-
-export { Home, NewRoom, Room, AdminRoom };
